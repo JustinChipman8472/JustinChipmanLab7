2f0c0233ba4a8b687e6ab186dc84886510e48064
JustinChipman8472 <n01598472@humber.ca>
2024-03-03 03:19:48 -0500

    added imagebutton and progress bar to home fragment with desired functionality

--- app/src/main/java/justin/chipman/n01598472/HomeFragment.java
+++ app/src/main/java/justin/chipman/n01598472/HomeFragment.java
@@ -2,15 +2,22 @@ package justin.chipman.n01598472;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 
 public class HomeFragment extends Fragment {
 
+    ProgressBar progressBar;
+    ImageButton imageButton;
+    int clickCounter;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -26,5 +33,31 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        progressBar = view.findViewById(R.id.progressBar);
+        imageButton = view.findViewById(R.id.imageButton);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickCounter++;
+                String input = getString(R.string.justin) + " " + clickCounter;
+                Toast.makeText(getActivity(), input, Toast.LENGTH_SHORT).show();
+                progressBar.setProgress(clickCounter);
+                if (clickCounter == 5) {
+                    progressBar.setVisibility(View.GONE);
+                }
+
+                if (clickCounter == 8) {
+                    getActivity().finish();
+                    System.exit(0);
+                }
+            }
+        });
     }
 }
