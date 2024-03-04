770fc48605d130debf1461200e1f149f41677c5a
JustinChipman8472 <n01598472@humber.ca>
2024-03-03 20:28:47 -0500

    Added an implemented the clock on settings fragment.

--- app/src/main/java/justin/chipman/n01598472/SettingsFragment.java
+++ app/src/main/java/justin/chipman/n01598472/SettingsFragment.java
@@ -1,30 +1,50 @@
 package justin.chipman.n01598472;
 
 import android.os.Bundle;
-
+import android.os.Handler;
+import android.os.Looper;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class SettingsFragment extends Fragment {
 
+    private TextView textViewClock;
+    private TextView textViewProvince;
+    private TextView textViewIndex;
 
     public SettingsFragment() {
         // Required empty public constructor
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        textViewClock = rootView.findViewById(R.id.textViewClock);
+        textViewProvince = rootView.findViewById(R.id.textViewProvince);
+        textViewIndex = rootView.findViewById(R.id.textViewIndex);
+
+        // Update clock every second
+        final Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                String currentTime = new SimpleDateFormat("HH:mm:ss", Locale.CANADA).format(new Date());
+                textViewClock.setText(currentTime);
+                handler.postDelayed(this, 1000);
+            }
+        }, 10);
+
+        // TODO: Retrieve and set the province and index from PersonFragment
+
+        return rootView;
     }
 }
